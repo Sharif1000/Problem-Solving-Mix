36ba32219e995d1ad33fae229035dd575721559d
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-25 10:54:07 +0600

    Create Problem_004.cpp

---
+++ Problem_004.cpp
@@ -0,0 +1,19 @@
+--Problem Link: https://codeforces.com/contest/2106/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--){
+      int n, x;
+      cin>>n>>x;
+      for(int i = 0;i < n; i++){
+        if(i != x) cout<<i<<" ";
+      }
+      if(x < n) cout<<x;
+      cout<<endl;
+    }
+    return 0;
+}
